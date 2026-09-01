96202e9725e5961b0b2a84e164c11662ecb57fe6
Nayak <srinuengr@gmail.com>
2026-09-01 11:39:03 -0400

    Add outputs for cluster endpoint and EBS CSI role ARN

--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output "cluster_id" {
   value = aws_eks_cluster.devopsshack.id
 }
 
+output "cluster_endpoint" {
+  value = aws_eks_cluster.devopsshack.endpoint
+}
+
 output "node_group_id" {
   value = aws_eks_node_group.devopsshack.id
 }
@@ -12,4 +16,8 @@ output "vpc_id" {
 
 output "subnet_ids" {
   value = aws_subnet.devopsshack_subnet[*].id
+}
+
+output "ebs_csi_role_arn" {
+  value = aws_iam_role.ebs_csi_driver_role.arn
 }
